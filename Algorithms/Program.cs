0189660b1548fc681ec64c9cc0e77d530a02872a
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2018-02-24 16:27:38 -0800

    Finished quicksort!

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -15,7 +15,7 @@ namespace Algorithms
             Console.WriteLine("");
             QuickSort(input);
             Console.Write("Output is: ");
-            for(int i = 0; i < input.Length; i++)
+            for (int i = 0; i < input.Length; i++)
             {
                 Console.Write(input[i].ToString() + ", ");
             }
@@ -41,11 +41,11 @@ namespace Algorithms
 
         public static int[] InsertionSort(int[] input)
         {
-            for(int i = 0; i < input.Length; i++)
+            for (int i = 0; i < input.Length; i++)
             {
-                if(i + 1 != input.Length)
+                if (i + 1 != input.Length)
                 {
-                    if(input[i] > input[i + 1])
+                    if (input[i] > input[i + 1])
                     {
                         for (int j = i + 1; j > 0; j--)
                         {
@@ -62,21 +62,16 @@ namespace Algorithms
             return input;
         }
 
-        public static int[] QuickSort(int[] input)
-        {
-            return QuickSort(input, 0, input.Length - 1);
-        }
+        public static void QuickSort(int[] input) => QuickSort(input, 0, input.Length - 1);
 
-        public static int[] QuickSort(int[] input, int leftIndex, int rightIndex)
+        public static void QuickSort(int[] input, int leftIndex, int rightIndex)
         {
-            if(leftIndex <= rightIndex)
+            if (leftIndex <= rightIndex)
             {
                 int pivotIndex = Partition(input, leftIndex, rightIndex);
-                input = QuickSort(input, leftIndex, pivotIndex - 1);
-                input = QuickSort(input, pivotIndex + 1, rightIndex);
+                QuickSort(input, leftIndex, pivotIndex - 1);
+                QuickSort(input, pivotIndex + 1, rightIndex);
             }
-
-            return input;
         }
 
         public static int Partition(int[] input, int leftIndex, int rightIndex)
@@ -84,19 +79,87 @@ namespace Algorithms
             int pivotValue = input[rightIndex];
 
             int leftSwap = leftIndex - 1;
-            for(int rightSwap = leftIndex; rightSwap <= rightIndex; rightSwap++)
+            for (int rightSwap = leftIndex; rightSwap <= rightIndex; rightSwap++)
             {
-                if(rightSwap <= pivotValue){
-                    leftSwap++;
-                    int temp = input[leftSwap];
-                    input[leftSwap] = input[rightSwap];
-                    input[rightSwap] = temp;
+                if (input[rightSwap] <= pivotValue)
+                {
+                    if (leftSwap != rightSwap)
+                    {
+                        leftSwap++;
+                        int temp = input[leftSwap];
+                        input[leftSwap] = input[rightSwap];
+                        input[rightSwap] = temp;
+                    }
                 }
-
-                return rightSwap;
             }
 
-            return 0;
+            return leftSwap;
+        }
+
+        public static int FindLargest(int[][] matrix, int range)
+        {
+            int highest = 0;
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; i++)
+                {
+                    int[] values = new int[4] { 0, 0, 0, 0 };
+
+                    values[0] = 1;
+                    foreach (int k in Enumerable.Range(j, j + range))
+                    {
+                        if (k <= matrix[i].Length)
+                            values[0] = values[0] * matrix[i][k];
+                    }
+
+                    values[1] = 1;
+                    foreach (int k in Enumerable.Range(j, j + range))
+                    {
+                        if (k <= matrix[i].Length)
+                        {
+                            foreach (int l in Enumerable.Range(i, i + range))
+                            {
+                                if (l <= matrix.Length)
+                                {
+                                    values[1] = values[1] * matrix[l][k];
+                                }
+                            }
+
+                            values[2] = values[2] * matrix[i][k];
+
+                            foreach (int m in Enumerable.Range(i - range, range))
+                            {
+                                if (m <= matrix.Length)
+                                {
+                                    values[3] = values[3] * matrix[m][k];
+                                }
+                            }
+                        }
+                    }
+
+                    if (values[0] > highest)
+                    {
+                        highest = values[0];
+                    }
+
+                    if (values[1] > highest)
+                    {
+                        highest = values[1];
+                    }
+
+                    if (values[2] > highest)
+                    {
+                        highest = values[2];
+                    }
+
+                    if (values[3] > highest)
+                    {
+                        highest = values[3];
+                    }
+                }
+            }
+
+            return highest;
         }
     }
 }
