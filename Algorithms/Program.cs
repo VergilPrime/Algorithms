2acadc6f0e40d911a939b9954e0f4a263d69048e
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2018-02-24 17:46:41 -0800

    Merge sort is working

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -13,7 +13,7 @@ namespace Algorithms
             int[] input = new int[] { 56, 32, 41, 66, 23, 1016, 41, 12 };
             Console.WriteLine("Input is: ' 56, 32, 41, 66, 23, 1016, 41, 12 '");
             Console.WriteLine("");
-            QuickSort(input);
+            MergeSort(input);
             Console.Write("Output is: ");
             for (int i = 0; i < input.Length; i++)
             {
@@ -160,6 +160,72 @@ namespace Algorithms
             }
 
             return highest;
+        }
+
+        public static void MergeSort(int[] input)
+        {
+            if(input.Length > 2)
+            {
+                int split = input.Length / 2;
+                int[] left = new int[split];
+                int[] right = new int[input.Length - split];
+                for (int i = 0; i < split; i++)
+                {
+                    left[i] = input[i];
+                }
+
+                for (int j = split; j < input.Length; j++)
+                {
+                    right[j - split] = input[j];
+                }
+
+                MergeSort(left);
+                MergeSort(right);
+
+                int leftindex = 0;
+                int rightindex = 0;
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if(leftindex < left.Length && rightindex < right.Length)
+                    {
+                        if(left[leftindex] < right[rightindex])
+                        {
+                            input[i] = left[leftindex];
+                            leftindex++;
+                        }
+                        else
+                        {
+                            input[i] = right[rightindex];
+                            rightindex++;
+                        }
+                    }
+                    else if(leftindex < left.Length && rightindex >= right.Length)
+                    {
+                        input[i] = left[leftindex];
+                        leftindex++;
+                    }
+                    else if(rightindex < right.Length && leftindex >= left.Length)
+                    {
+                        input[i] = right[rightindex];
+                        rightindex++;
+                    }
+                    else if(leftindex >= left.Length && rightindex >= right.Length)
+                    {
+                        Console.Write("Something went terribly wrong.");
+                    }
+                }
+
+            }else if(input.Length == 2)
+            {
+                if(input[0] > input[1])
+                {
+                    int temp = input[0];
+                    input[0] = input[1];
+                    input[1] = temp;
+                }
+            }
+
         }
     }
 }
